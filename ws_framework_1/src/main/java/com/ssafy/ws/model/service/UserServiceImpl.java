5b58029192413ad2d123c7be6061ec798685b04b
Kathleen Jung <ovo0325@outlook.kr>
2022-10-18 11:26:06 +0900

    ws_framework_1 초기 파일 업로드

---
+++ ws_framework_1/src/main/java/com/ssafy/ws/model/service/UserServiceImpl.java
@@ -0,0 +1,27 @@
+package com.ssafy.ws.model.service;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+
+import com.ssafy.ws.dto.User;
+import com.ssafy.ws.model.repo.UserRepo;
+
+
+public class UserServiceImpl implements UserService {
+
+	private UserRepo userRepo;
+	
+	
+	public UserServiceImpl(UserRepo repo) {
+		this.userRepo = repo;
+	}
+
+
+
+	@Override
+	public User select(String id) {
+		// TODO Auto-generated method stub
+		return null;
+	}
+
+}
