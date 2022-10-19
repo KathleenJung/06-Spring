61c24d445f2a32a3204ce2573389686bcd319035
Kathleen Jung <ovo0325@outlook.kr>
2022-10-19 09:09:11 +0900

    spring_webmvc import

--- ws_framework_1/src/main/java/com/ssafy/ws/model/repo/UserRepo.java
+++ ws_framework_1/src/main/java/com/ssafy/ws/model/repo/UserRepo.java
@@ -1,7 +1,9 @@
 package com.ssafy.ws.model.repo;
 
+import java.sql.SQLException;
+
 import com.ssafy.ws.dto.User;
 
 public interface UserRepo {
-	User select(String id);
+	User select(String id) throws SQLException;
 }
--- ws_framework_1/src/main/java/com/ssafy/ws/model/repo/UserRepoImpl.java
+++ ws_framework_1/src/main/java/com/ssafy/ws/model/repo/UserRepoImpl.java
@@ -1,15 +1,25 @@
 package com.ssafy.ws.model.repo;
 
+import java.sql.SQLException;
+
+import javax.sql.DataSource;
+
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Repository;
 
 import com.ssafy.ws.dto.User;
 
 @Repository
 public class UserRepoImpl implements UserRepo {
-
+	
+	@Autowired
+	DataSource ds;
+	
 	@Override
-	public User select(String id) {
-		// TODO Auto-generated method stub
+	public User select(String id) throws SQLException {
+		User u = new User();
+		String sql = "SELECT id, name, pass, rec_id FROM user where id = ? ";
+		ds.getConnection();
 		return null;
 	}
 
