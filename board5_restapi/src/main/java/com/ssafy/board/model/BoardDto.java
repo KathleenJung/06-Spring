77578855568504f9635fa6306e026f84f0c4565d
Kathleen Jung <ovo0325@outlook.kr>
2022-10-28 10:44:03 +0900

    Use Lombok Getter & Setter

--- board5_restapi/src/main/java/com/ssafy/board/model/BoardDto.java
+++ board5_restapi/src/main/java/com/ssafy/board/model/BoardDto.java
@@ -2,6 +2,11 @@ package com.ssafy.board.model;
 
 import java.util.List;
 
+import lombok.Getter;
+import lombok.Setter;
+
+@Setter
+@Getter
 public class BoardDto {
 
 	private int articleNo;
@@ -12,70 +17,6 @@ public class BoardDto {
 	private int hit;
 	private String registerTime;
 	private List<FileInfoDto> fileInfos;
-
-	public int getArticleNo() {
-		return articleNo;
-	}
-
-	public void setArticleNo(int articleNo) {
-		this.articleNo = articleNo;
-	}
-
-	public String getUserId() {
-		return userId;
-	}
-
-	public void setUserId(String userId) {
-		this.userId = userId;
-	}
-
-	public String getUserName() {
-		return userName;
-	}
-
-	public void setUserName(String userName) {
-		this.userName = userName;
-	}
-
-	public String getSubject() {
-		return subject;
-	}
-
-	public void setSubject(String subject) {
-		this.subject = subject;
-	}
-
-	public String getContent() {
-		return content;
-	}
-
-	public void setContent(String content) {
-		this.content = content;
-	}
-
-	public int getHit() {
-		return hit;
-	}
-
-	public void setHit(int hit) {
-		this.hit = hit;
-	}
-
-	public String getRegisterTime() {
-		return registerTime;
-	}
-
-	public void setRegisterTime(String registerTime) {
-		this.registerTime = registerTime;
-	}
-
-	public List<FileInfoDto> getFileInfos() {
-		return fileInfos;
-	}
-
-	public void setFileInfos(List<FileInfoDto> fileInfos) {
-		this.fileInfos = fileInfos;
-	}
 
 	@Override
 	public String toString() {
