61999bd9e94e584f762dae7dd81f56fe50632ed6
Kathleen Jung <ovo0325@outlook.kr>
2022-10-28 10:53:08 +0900

    Use Lombok Getter & Setter

--- board5_restapi/src/main/java/com/ssafy/board/model/BoardDto.java
+++ board5_restapi/src/main/java/com/ssafy/board/model/BoardDto.java
@@ -2,15 +2,27 @@ package com.ssafy.board.model;
 
 import java.util.List;
 
+import lombok.AllArgsConstructor;
 import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.NonNull;
+import lombok.RequiredArgsConstructor;
 import lombok.Setter;
+import lombok.ToString;
 
 @Setter
 @Getter
+@NoArgsConstructor		// 기본 생성자
+@AllArgsConstructor 	// 모든 변수를 사용하는 생성자
+@RequiredArgsConstructor// 특정 변수를 활용해서 생성자를 자동 생성
+@ToString
 public class BoardDto {
 
+	@NonNull
 	private int articleNo;
+	@NonNull
 	private String userId;
+	@NonNull
 	private String userName;
 	private String subject;
 	private String content;
