5c1d0b32306d2e69978573e12e55600a52710094
Kathleen Jung <ovo0325@outlook.kr>
2022-10-31 16:55:43 +0900

    spring boot project + swagger

--- HelloSpringBoot/src/main/java/com/ssafy/hello/controller/HelloRestController.java
+++ HelloSpringBoot/src/main/java/com/ssafy/hello/controller/HelloRestController.java
@@ -1,11 +1,13 @@
 package com.ssafy.hello.controller;
 
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RestController;
 
+@RestController
 public class HelloRestController {
 
 	@GetMapping("/resthello")
 	public String hello() {
-		return "Hello Rest Controller";
+		return "Hello Rest Controller!!!!";
 	}
 }
