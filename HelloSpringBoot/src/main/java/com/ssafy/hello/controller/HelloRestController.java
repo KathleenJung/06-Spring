35ee0c4331ad02571c004add97edac58d0128818
Kathleen Jung <ovo0325@outlook.kr>
2022-10-31 14:28:04 +0900

    hello spring boot

--- HelloSpringBoot/src/main/java/com/ssafy/hello/controller/HelloRestController.java
+++ HelloSpringBoot/src/main/java/com/ssafy/hello/controller/HelloRestController.java
@@ -1,5 +1,11 @@
 package com.ssafy.hello.controller;
 
+import org.springframework.web.bind.annotation.GetMapping;
+
 public class HelloRestController {
 
+	@GetMapping("/resthello")
+	public String hello() {
+		return "Hello Rest Controller";
+	}
 }
